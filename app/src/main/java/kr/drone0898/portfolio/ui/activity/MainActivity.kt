7dac4bcb0202992d77c756e0961fbc7807d95f8d
drone0898 <drone0898@gmail.com>
2020-10-13 10:32:23 +0900

    Initial commit

---
+++ app/src/main/java/kr/drone0898/portfolio/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.drone0898.portfolio.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.drone0898.portfolio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
